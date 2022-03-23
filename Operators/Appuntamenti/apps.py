21f2b694d04fb0e8417d14e4dd6cca3834d66470
Potatosauro <98522688+Potatosauro@users.noreply.github.com>
2022-03-23 18:33:29 +0100

    Delete Operators directory

--- Operators/Appuntamenti/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppuntamentiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Appuntamenti'
--- Operators/Appuntamenti/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import Operator
-urlpatterns = [
-    path('', Operator.as_view(),name="operator"),
-]
